91dc32312009e4202cda4f9d25390d306350926f
Zeioth <zeioth@hotmail.com>
2024-03-21 18:13:19 +0100

    ♻️refactor: All mason registries must go inside the plugin mason to avoid funny stuff.

--- lua/plugins/3-dev-core.lua
+++ lua/plugins/3-dev-core.lua
@@ -188,15 +188,7 @@ return {
       "MunifTanjim/nui.nvim",
       "neovim/nvim-lspconfig",
       "mfussenegger/nvim-dap",
-      {
-        "williamboman/mason.nvim",
-        opts = {
-          registries = {
-            "github:nvim-java/mason-registry",
-            "github:mason-org/mason-registry",
-          },
-        },
-      }
+      "williamboman/mason.nvim",
     },
     opts = {
 	    notifications = {
@@ -255,6 +247,10 @@ return {
       "MasonUpdateAll", -- this cmd is provided by mason-extra-cmds
     },
     opts = {
+      registries = {
+        "github:nvim-java/mason-registry",
+        "github:mason-org/mason-registry",
+      },
       ui = {
         icons = {
           package_installed = "✓",
